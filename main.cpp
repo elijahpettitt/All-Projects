0226ca1e04fa47d2e7bc9d445d7bab46d9031ce1
Elijah Pettitt <elijahpettitt@gmail.com>
2021-01-26 04:16:02 +0000

    with default main, work on paramaters next

--- main.cpp
+++ main.cpp
@@ -1,47 +1,160 @@
+//A Note regarding 'using namespace std':
+// The author of this code prefers to not have the line 'using namespace std;'
+// Instead, you will see 'std::' in front of string, cout, and other identifiers
+// You could put 'using namespace std;' at the top of your code and not need 'std::' in front of these words,
+// but many people consider it better practice to not have 'using namespace std;'
+
 #include <iostream>
-#include <iomanip> 
-#include "ItemToPurchase.h"
+#include <string>
+
 #include "ShoppingCart.h"
-using namespace std;
+
+//TODO: Include your class files here
+
+std::string GetUserString(const std::string& prompt);
+
+void AddMenuOption(/*TODO: list any parameters here*/);
+void RemoveMenuOption(/*TODO: list any parameters here*/);
+void ChangeMenuOption(/*TODO: list any parameters here*/);
+void DescriptionsMenuOption(/*TODO: list any parameters here*/);
+void CartMenuOption(/*TODO: list any parameters here*/);
+void OptionsMenuOption();
+void QuitMenuOption();
+
 
 int main() {
-  ItemToPurchase itemToP1;
-//	ItemToPurchase itemToP2;
-	//string name = "";
-	//double price = 0.0;
-	//int quantity = 0;
+   std::string customerName = GetUserString("Enter Customer's Name: ");
+   std::string todayDate = GetUserString("Enter Today's Date: ");
 
-	cout << fixed << setprecision(2);
+   //TODO: Create a shopping cart with the above information
+   // .
+   // This should be just one line of code that calls your ShoppingCart constructor
+	 ShoppingCart cart(customerName, todayDate);
 
-	ItemToPurchase water("Water", "pure", 1.99, 3);
-	ItemToPurchase bread("Bread", "wheat", 2, 1);
+   std::string userMenuChoice = "none";
+   bool continueMenuLoop = true;
+   while (continueMenuLoop) {
+      userMenuChoice = GetUserString("Enter option: ");
 
-	cout << endl;
+      if (userMenuChoice == "add") {
+         AddMenuOption(/*TODO: list any arguments here*/);
+      }
+      else if (userMenuChoice == "remove") {
+         RemoveMenuOption(/*TODO: list any arguments here*/);
+      }
+      else if (userMenuChoice == "change") {
+         ChangeMenuOption(/*TODO: list any arguments here*/);
+      }
+      else if (userMenuChoice == "descriptions") {
+         DescriptionsMenuOption(/*TODO: list any arguments here*/);
+      }
+      else if (userMenuChoice == "cart") {
+         CartMenuOption(/*TODO: list any arguments here*/);
+      }
+      else if (userMenuChoice == "options") {
+         OptionsMenuOption();
+      }
+      else if (userMenuChoice == "quit") {
+         QuitMenuOption();
+         continueMenuLoop = false;
+      }
+      else {
+         OptionsMenuOption();
+      }
+   }
 
-	//ShoppingCart cart("The Cart", "Jan 12, 2020");
-	ShoppingCart cart;
-
-	//cout << "empty1: " << endl;
-	//cart.PrintTotalQuantityAndCost();
-	//cart.PrintItems();
-
-	cart.AddItem(water);
-	cart.AddItem(bread);
-
-	cout << "full2: " << endl;
-	cart.PrintTotalQuantityAndCost();
-	cout << endl << endl << endl;
-	cart.PrintItems();
+   return 0;
+}
 
 
-//	cout << cart.GetCustomerName() << endl;
-//	cout << cart.GetCreationDate() << endl;
+std::string GetUserString(const std::string& prompt) {
+   std::string userAnswer = "none";
 
-	//cart.UpdateItemQuantity("Water", 0);
-	//cart.UpdateItemQuantity("Bread", 0);
+   std::cout << prompt;
+   std::getline(std::cin, userAnswer);
+   std::cout << std::endl;
+   return userAnswer;
+}
+double GetUserDouble(const std::string& prompt) {
+   double userAnswer = 0.0;
 
-	cout << "empty3: " << endl;
+   std::cout << prompt;
+   std::cin >> userAnswer;
+   std::cin.ignore();
+   std::cout << std::endl;
+   return userAnswer;
+}
+int GetUserInt(const std::string& prompt) {
+   int userAnswer = 0;
 
-	//cart.PrintTotalQuantityAndCost();
-	//cart.PrintItems();
+   std::cout << prompt;
+   std::cin >> userAnswer;
+   std::cin.ignore();
+   std::cout << std::endl;
+   return userAnswer;
+}
+
+
+void AddMenuOption(/*TODO: list any parameters here*/) {
+   std::string itemName = GetUserString("Enter the item name: ");
+   std::string itemDescription = GetUserString("Enter the item description: ");
+   double itemPrice = GetUserDouble("Enter the item price: ");
+   int itemQuantity = GetUserInt("Enter the item quantity: ");
+
+   //TODO: Create an item and add it to the shopping cart,
+   // storing whether or not the item was added successfully
+   // (the return value of your function) in 'itemAddedSuccessfully'
+   // .
+   // This should be just 1-2 lines of code to create the item and call the appropriate method on the shoppingCart
+   // You might also print error messages here, depending on how you implement the ShoppingCart class
+}
+
+void RemoveMenuOption(/*TODO: list any parameters here*/) {
+   std::string nameOfItemToRemove = GetUserString("Enter name of the item to remove: ");
+
+   //TODO: Remove the item from the shopping cart,
+   // storing whether or not the item was removed successfully
+   // (the return value of your function) in 'itemRemovedSuccessfully'
+   // .
+   // This should be just 1 line of code to call the appropriate method on the shoppingCart
+   // You might also print error messages here, depending on how you implement the ShoppingCart class
+}
+
+void ChangeMenuOption(/*TODO: list any parameters here*/) {
+   std::string nameOfItemToChange = GetUserString("Enter the item name: ");
+   int newItemQuantity = GetUserInt("Enter the new quantity: ");
+
+   //TODO: Change the quantity of the item in the shopping cart,
+   // storing whether or not the item was modified successfully
+   // (the return value of your function) in 'itemModifiedSuccessfully'
+   // .
+   // This should be just 1 line of code to call the appropriate method on the shoppingCart
+   // You might also print error messages here, depending on how you implement the ShoppingCart class
+}
+
+void DescriptionsMenuOption(/*TODO: list any parameters here*/) {
+   //TODO: Print the information from the shopping cart
+   // .
+   // This should be just 1 line of code to call the appropriate method on the shoppingCart
+}
+
+void CartMenuOption(/*TODO: list any parameters here*/) {
+   //TODO: Print the information from the shopping cart
+   // .
+   // This should be just 1 line of code to call the appropriate method on the shoppingCart
+}
+
+void OptionsMenuOption() {
+   std::cout << "MENU" << std::endl
+             << "add - Add item to cart" << std::endl
+             << "remove - Remove item from cart" << std::endl
+             << "change - Change item quantity" << std::endl
+             << "descriptions - Print the items' descriptions" << std::endl
+             << "cart - Print the shopping cart" << std::endl
+             << "options - Print the options menu" << std::endl
+             << "quit - Quit" << std::endl << std::endl;
+}
+
+void QuitMenuOption() {
+   std::cout << "Goodbye." << std::endl;
 }
